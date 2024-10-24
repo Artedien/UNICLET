3146b8439da705364cbccb686cd5a57869801a91
artedien <artedien@gmail.com>
2024-10-24 18:14:16 +0300

    fix filter

--- src/js/components/filterModal.js
+++ src/js/components/filterModal.js
@@ -1,18 +1,21 @@
 function filterModal() {
-    const btn = document.querySelector('.main__tab-status-title-status-image');
-    const modal = document.querySelector('.filter__modal-wrapper');
-    const closeBtn = document.querySelector('.filter__modal-close-box')
+    const btns = document.querySelectorAll('.main__tab-status-title-status-image');
+    const modal = document.querySelectorAll('.filter__modal-wrapper');
+    const closeBtn = document.querySelectorAll('.filter__modal-close-box')
 
+    btns.forEach((btn,i)=>{
+        btn.addEventListener('click', () => {
+            modal[i].classList.toggle('active');
+        
+        });
+        closeBtn[i].addEventListener('click', () => {
+            modal[i].classList.remove('active');
     
-    btn.addEventListener('click', () => {
-        modal.classList.toggle('active');
-    
-    });
+        });
+    })
+ 
 
-    closeBtn.addEventListener('click', () => {
-        modal.classList.remove('active');
 
-    });
 }
 
 export default filterModal;
